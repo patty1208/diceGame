c843a3d2d7dbdb4c8fc5ece39b4d211bb31fb6c5
patty <patty99491208@gmail.com>
2021-02-23 15:45:17 +0800

    Initial Commit

---
+++ diceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceGame
+//
+//  Created by 林佩柔 on 2021/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
